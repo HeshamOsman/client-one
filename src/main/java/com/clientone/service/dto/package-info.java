357592e8f2d2d8e7a3da96082e6701e7d32abc73
HeshamOsman <hesham.osman28@yahoo.com>
2019-07-18 02:18:56 -0700

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/com/clientone/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.clientone.service.dto;
---
+++ src/main/java/com/clientone/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.clientone.web.rest.vm;
